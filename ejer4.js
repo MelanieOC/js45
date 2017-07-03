2cd493c4115dc94830905282ca794251d42a21ad
MelanieOC <melanie.ocharan@gmail.com>
2017-07-03 16:38:39 -0500

    ya

--- ejer4.js
+++ ejer4.js
@@ -7,21 +7,13 @@ function Dibujar(n) {
   return s;
 }
 
-for (var i = 0; i < n; i++) {
-  var sp = espacios (n - (i+1));
+for (var k = 0; k < 2 * n - 1; k++) {
+  var i = k < n ? k :  2*n - 2 - k;
   var content = "";
-  for (j = 1; j <  i+i; j++)
+  for (j = 1; j <= 2*i+1; j++)
     content +=  j;
-  console.log(sp + content);
-}
-for (var i = n-2; i >= 0; i--) {
   var sp = espacios (n - (i+1));
-  var content = "";
-  for (j = 1; j < i+i; j++)
-    content +=  j;
   console.log(sp + content);
-}
-
 }
 
 Dibujar(5);
